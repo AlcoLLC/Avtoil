a7a32f32675bf30902afdd62218573ad6ab49826
Aytac Mehdizadə <114391044+Aytac21@users.noreply.github.com>
2025-09-19 17:35:44 +0400

    contact

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,8 @@
+# contact/forms.py
+
 from django import forms
 from .models import Contact
-
+from django.utils.translation import gettext_lazy as _
 
 class ContactForm(forms.ModelForm):
     class Meta:
@@ -9,15 +11,13 @@ class ContactForm(forms.ModelForm):
                   'last_name', 'email', 'phone_number']
         
     def __init__(self, *args, **kwargs):
-        super(ContactForm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         
-        for field_name, field in self.fields.items():
-            field.required = True
-            
-        self.fields['help_type'].label = "How can we help you?*"
-        self.fields['company_name'].label = "Company name*"
-        self.fields['question'].label = "Your question, wish and/or clarification*"
-        self.fields['first_name'].label = "First name*"
-        self.fields['last_name'].label = "Last name*"
-        self.fields['email'].label = "Email address*"
-        self.fields['phone_number'].label = "Phone number*"
+        # Etiketleri burada tanımlamak daha iyi bir pratiktir
+        self.fields['help_type'].label = _("How can we help you?*")
+        self.fields['company_name'].label = _("Company name*")
+        self.fields['question'].label = _("Your question, wish and/or clarification*")
+        self.fields['first_name'].label = _("First name*")
+        self.fields['last_name'].label = _("Last name*")
+        self.fields['email'].label = _("Email address*")
+        self.fields['phone_number'].label = _("Phone number*")

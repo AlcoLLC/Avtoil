bdc38b7d733df90752e3be7ead44ae3f661f75d7
Aytac Mehdizadə <114391044+Aytac21@users.noreply.github.com>
2025-10-20 09:59:48 +0400

    meta_tags

--- products/models.py
+++ products/models.py
@@ -8,7 +8,6 @@ class Product_group(models.Model):
     slug = models.SlugField(max_length=255, unique=True)
     in_home = models.BooleanField(default=False, verbose_name="In Home")
     order = models.IntegerField(default=0)
-    
     meta_title = models.CharField(max_length=255, blank=True, null=True)
     meta_description = models.TextField(blank=True, null=True, verbose_name="Meta Description")
     
@@ -20,9 +19,8 @@ class Product_Group_Category(models.Model):
     title = RichTextUploadingField(blank=True, null=True) 
     description = RichTextUploadingField(blank=True, null=True) 
 
-
     def __str__(self):
-        return f"{self.product_group.title}"
+        return f"{self.title}"
     
 class Segments(models.Model):
     title = models.CharField(max_length=255)
